57a60efa5c770a1c93901a59145ab92a6da2c16d
vinurs <haiyuan.vinurs@gmail.com>
2018-06-22 10:56:42 +0800

    :pencil: 初始化项目

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to netease-im-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/netease_im_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns netease-im-clj.core-test
+  (:require [clojure.test :refer :all]
+            [netease-im-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
